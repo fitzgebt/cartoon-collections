9b308cc114f0c104bd84a948cb84d1a9340c348d
fitzgebt <fitzgebt@gmail.com>
2021-01-15 12:08:11 -0800

    commit tree 18a1afc4a0e71e704772847b71065aed5650b732

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(array)
+  i = 0
+  while i < array.length
+    print "#{i+1}. #{array[i]}"
+    i+=1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map! {|word| "#{word.capitalize}!"}
+  return array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(argument)
+  argument.any?{|long| long.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.find{|cheese| return cheese if array.include?(cheese)}
 end
